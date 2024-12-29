b0798eb5765bba4c6f43a83d0226c6100c113bd1
TaitA2 <adam.tait617@gmail.com>
2024-12-29 02:52:59 +0000

    finish day 11

--- day_11/python/part_2.py
+++ day_11/python/part_2.py
@@ -2,36 +2,45 @@ import time
 
 
 def main():
-    rocks = input().split()
+    rocks = get_rocks()
     total = solve(rocks, 75)
     print(total)
 
 
 def solve(rocks, n):
-    done = {}
-    for i in range(n):
-        start = time.time()
-        rocks, done = update(rocks, done)
-        with open("out.txt", "a") as f:
-            f.write(f"{i}: {time.time() - start:.2f} seconds\n")
-    return len(rocks)
+    for _ in range(n):
+        rocks = update(rocks)
+    return sum(rocks.values())
 
 
-def update(rocks, done):
-    updated = []
-    for rock in rocks:
-        if rock in done:
-            updated_rock = done[rock]
-        elif rock == "0":
-            updated_rock = ["1"]
+def update(rocks):
+    done = {"1": 0}
+    for rock, count in rocks.items():
+        if rock == "0":
+            updated_rock = "1"
         elif len(rock) % 2 == 0:
             n = len(rock) // 2
-            updated_rock = [rock[:n], str(int(rock[n:]))]
+            updated_rock, updated_rock2 = rock[:n], str(int(rock[n:]))
+            if updated_rock2 not in done:
+                done[updated_rock2] = 0
+            done[updated_rock2] += count
         else:
-            updated_rock = [str(2024 * int(rock))]
-        done[rock] = updated_rock
-        updated += done[rock]
-    return updated, done
+            updated_rock = str(2024 * int(rock))
+        if updated_rock not in done:
+            done[updated_rock] = 0
+        done[updated_rock] += count
+    return done
+
+
+def get_rocks():
+
+    a = input().split()
+
+    rocks = {}
+
+    for rock in a:
+        rocks[rock] = 1
+    return rocks
 
 
 if __name__ == "__main__":
