c9054432cbd8830d358a85d3aba1adba1025874c
Adam btw <adam.tait617@gmail.com>
2025-12-17 19:28:01 +0000

    finish day 1 in go

---
+++ 2025/day_1/go/go.mod
@@ -0,0 +1,3 @@
+module github.com/TaitA2/adventofcode/2025/day_1
+
+go 1.25.1
---
+++ 2025/day_1/go/main.go
@@ -0,0 +1,61 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func get_rotations() []string {
+	rotations, err := os.ReadFile("../input.txt")
+	if err != nil {
+		log.Fatalf("Error reading file: %v", err)
+	}
+
+	return strings.Split(string(rotations), "\n")
+
+}
+
+func rotate_dial(rotations []string, start int, max int) int {
+	zero_count := 0
+	pos := start
+
+	for _, r := range rotations {
+		if len(r) < 1 {
+			break
+		}
+		d := r[:1]
+		n, err := strconv.Atoi(r[1:])
+		if err != nil {
+			log.Fatalf("Error converting rotation to int: %v", err)
+		}
+		switch d {
+		case "L":
+			pos -= (n % max)
+			if pos < 0 {
+				pos += max
+			}
+		case "R":
+			pos += (n % max)
+			for pos >= max {
+				pos -= max
+			}
+
+		}
+		if pos == 0 {
+			zero_count++
+		}
+	}
+
+	return zero_count
+}
+
+func main() {
+	rotations := get_rotations()
+	start := 50
+	max := 100
+	ans := rotate_dial(rotations, start, max)
+	fmt.Println(ans)
+}
