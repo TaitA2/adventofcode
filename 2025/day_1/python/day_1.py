0c94466730e3384501fee8fb01ae91766a93af08
Adam btw <adam.tait617@gmail.com>
2025-12-30 16:05:11 +0000

    finish day 1 part 2 in python

--- 2025/day_1/python/day_1.py
+++ 2025/day_1/python/day_1.py
@@ -3,7 +3,7 @@ def get_rotations():
         rotations = list(f.read().split())
     return rotations
 
-def rotate_dial(rotations, start,max,zero_count):
+def rotate_dial_1(rotations, start,max,zero_count):
     pos = start
     for rotation in rotations:
         direction, n = rotation[0], int(rotation[1:])
@@ -22,17 +22,44 @@ def rotate_dial(rotations, start,max,zero_count):
             zero_count += 1
     return zero_count
 
+def rotate_dial_2(rotations, start,max,zero_count):
+    pos = start
+    for rotation in rotations:
+        direction, n = rotation[0], int(rotation[1:])
+
+        match direction:
+            case "L":
+                if pos == 0:
+                    zero_count -= 1
+                pos -= n
+                while pos < 0:
+                    pos += (max+1)
+                    zero_count += 1
+            case "R":
+                pos += n
+                while pos > max:
+                    pos -= (max+1)
+                    if pos != 0:
+                        zero_count += 1
+        if pos == 0:
+            zero_count += 1
+
+    return zero_count
+
 
 def main():
     max = 99
     zero_count = 0
     start = 50
-    n = start
     rotations = get_rotations()
 
-    ans = rotate_dial(rotations,start,max,zero_count)
+    ans = rotate_dial_1(rotations,start,max,zero_count)
 
-    print(ans)
+    print("Part 1: ", ans)
+
+    ans = rotate_dial_2(rotations,start,max,zero_count)
+
+    print("Part 2: ", ans)
 
 
 if __name__ == "__main__":
